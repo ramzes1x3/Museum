5a1c897775f6c342c790b0073ebf6ab61ea24371
ramzes1x3 <ramzes1x1@gmail.com>
2022-02-16 00:35:45 +0300

    fix header menu

--- source/js/app.js
+++ source/js/app.js
@@ -1,37 +1,5 @@
 import * as flsFunction from "./modules/finder_webp.js";
 import * as functions from "./modules/functions.js";
+import * as swiper from "./modules/swiper.js";
 
 flsFunction.isWebp();
-functions.menuSwitch();
-
-let swiper = new Swiper(".swiper", {
-  watchOverflow: true,
-  loop: true,
-  loopFillGroupWithBlank: true,
-  effect: 'coverflow',
-  coverflowEffect:{
-    rotate: 30,
-    stretch: 0,
-    slideShadows: true,
-  },
-  breakpoints:{
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 10,
-      slidesPerGroup: 1,
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    },
-    767.98: {
-      slidesPerView: 3,
-      spaceBetween: 0,
-      slidesPerGroup: 1,
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    },
-  },
-});
--- source/js/modules/functions.js
+++ source/js/modules/functions.js
@@ -1,18 +1,33 @@
-export function menuSwitch() {
-  const arrowBtn = document.querySelector('.link--exposition');
+let hamburger = document.querySelector('.link--exposition');
+let expositionDesktop = document.querySelector('.list-exposition');
+let expositionMobile = document.querySelector('.list-exposition-mobile');
 
-  if(arrowBtn){
-    const expositionMenu = document.querySelector('.list-exposition');
-    const expositionParent = document.querySelector('.item--expostion-mobile');
-    const expositionMobile = document.querySelector('.list-exposition-mobile');
-
-    arrowBtn.addEventListener("click", function(e) {
-      e.preventDefault();
-      arrowBtn.classList.toggle('_active');
-      expositionParent.classList.toggle('_active');
-      expositionMenu.classList.toggle('_active');
-      expositionMobile.classList.toggle('_active');
-    });
-
-  }
+export const menuSwitch = () => {
+  expositionDesktop.classList.toggle('_active');
 }
+
+export const menuSwitchMobile = () =>{
+  expositionMobile.classList.toggle('_active');
+}
+
+hamburger.addEventListener('click', e => {
+  e.stopPropagation();
+  menuSwitch();
+  menuSwitchMobile();
+});
+
+document.addEventListener('click', e => {
+  let target = e.target;
+  let its_menu = target == expositionDesktop || expositionDesktop.contains(target);
+  let its_menu_mobile = target == expositionMobile || expositionMobile.contains(target);
+  let its_hamburger = target == hamburger;
+  let menu_is_active = expositionDesktop.classList.contains('_active');
+  let mobile_menu_is_active = expositionMobile.classList.contains('_active');
+
+  if (!its_menu && !its_hamburger && menu_is_active) {
+    menuSwitch();
+  }
+  if(!its_menu_mobile && !its_hamburger && mobile_menu_is_active){
+    menuSwitchMobile();
+  }
+})
