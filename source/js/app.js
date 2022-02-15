ec196f95f10ab8289a4fa3e7868e98d4190f1826
ramzes1x3 <ramzes1x1@gmail.com>
2022-02-15 11:34:20 +0300

    added slider

--- source/js/app.js
+++ source/js/app.js
@@ -5,20 +5,33 @@ flsFunction.isWebp();
 functions.menuSwitch();
 
 let swiper = new Swiper(".swiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  slidesPerGroup: 1,
   watchOverflow: true,
   loop: true,
   loopFillGroupWithBlank: true,
-  effect: 'coverflow',
-  coverflowEffect:{
-    rotate: 20,
-    stretch: 50,
-    slideShadows: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+  // effect: 'coverflow',
+  // coverflowEffect:{
+  //   rotate: 20,
+  //   stretch: 50,
+  //   slideShadows: true,
+  // },
+  breakpoints:{
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      slidesPerGroup: 1,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    },
+    767.98: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      slidesPerGroup: 1,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    },
   },
 });
