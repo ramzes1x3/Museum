23cc3a4fc39329ee8c9d328745fe76b4bcd78c0f
ramzes1x3 <ramzes1x1@gmail.com>
2022-05-09 14:43:39 +0300

    fix expo

--- source/js/app.js
+++ source/js/app.js
@@ -2,8 +2,6 @@ import * as flsFunction from "./modules/finder_webp.js";
 import * as functions from "./modules/switch_menu.js";
 import * as swiper from "./modules/swiper.js";
 import * as yandexMap from "./modules/yandex_map_api.js";
-import * as switchPopup from "./modules/toggle_popup_questions.js";
-import * as validFormMobile from "./modules/valid_form_mobile.js";
 import * as event from "./modules/events.js";
 
 flsFunction.isWebp();
